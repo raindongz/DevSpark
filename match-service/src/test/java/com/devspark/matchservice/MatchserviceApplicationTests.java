23e0ec415602c7c97e19b29ffb639cf16f80d4fc
RyanZhong <ryandzhong@gmail.com>
2024-03-09 19:59:19 -0500

    project initialized

---
+++ match-service/src/test/java/com/devspark/matchservice/MatchserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devspark.matchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/main/java/com/devspark/userservice/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.devspark.userservice.controller;
+
+public class UserController {
+}
