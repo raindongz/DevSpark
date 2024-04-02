5651dfeee2de41d499cd927ce48931a14cafa03a
rundongzhong <ryandzhong@gmail.com>
2024-04-02 18:53:18 -0400

    config open feign configuration

--- auth-service/src/main/java/com/devspark/authservice/service/FeignClientForUserService.java
+++ auth-service/src/main/java/com/devspark/authservice/service/FeignClientForUserService.java
@@ -5,8 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(url = "http://localhost:8082/api/user-info", value = "user-micro-service")
+//@FeignClient(url = "http://localhost:8082", value = "user-service")
+@FeignClient("user-service")
 public interface FeignClientForUserService {
-    @PostMapping("/save-user-info")
+    @PostMapping("/api/user-info/save-user-info")
     CreateUserProfileDTO saveUserInfo(@RequestBody CreateUserProfileDTO createUserProfileDTO);
 }
